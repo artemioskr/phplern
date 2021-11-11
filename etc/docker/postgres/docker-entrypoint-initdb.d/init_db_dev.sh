dccaffe5dbc6a191a17ebfefe8d79a376fc29f8b
Artemiy Skryabin <artemio.skr@gmail.com>
2021-11-11 15:34:27 +0300

    Revert "clone phpls"
    This reverts commit 29a247367bbf9783960fc6ed5c504419ff2e098d.

--- etc/docker/postgres/docker-entrypoint-initdb.d/init_db_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-   CREATE USER dev password '123' superuser;
-   CREATE DATABASE postgres owner dev LC_COLLATE='ru_RU.UTF-8' LC_CTYPE='ru_RU.UTF-8' TEMPLATE template0;
-EOSQL
-export PGDATA=/var/lib/postgresql/data
